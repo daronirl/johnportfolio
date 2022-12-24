02401ddb49e2744435e20dfdde14102a160b2733
jp <48197513+daronirl@users.noreply.github.com>
2022-12-23 21:54:42 -0800

    added smooth effects to sections

--- js/app.js
+++ js/app.js
@@ -3,6 +3,17 @@ const myCert = document.getElementById('myCert');
 const certPic = document.getElementById('certPic');
 
 
+// Added smooth scroll effect to sections
+document.querySelector('.nav').addEventListener('click', function(e) {
+    e.preventDefault();
+  
+      if (e.target.classList.contains('nav-link')) {
+        const id = e.target.getAttribute('href');
+        document.querySelector(id).scrollIntoView({behavior: 'smooth'});
+       console.log(id);
+      }
+  });
+
 // function to click to view certificate
 myCert.addEventListener('click', () => {
     if (myCert.style.display = 'none') {
